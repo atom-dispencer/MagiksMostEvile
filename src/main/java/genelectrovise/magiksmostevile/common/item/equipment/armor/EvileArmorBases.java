e9f8a0b3a9a704fb58e4c2f3de5c43efff87cb9e
GenElectrovise <sanchez.pamelos@outlook.com>
2020-06-23 15:30:49 +0100

    Fixed flashing lights while wearing armor

--- src/main/java/genelectrovise/magiksmostevile/common/item/equipment/armor/EvileArmorBases.java
+++ src/main/java/genelectrovise/magiksmostevile/common/item/equipment/armor/EvileArmorBases.java
@@ -10,7 +10,7 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.world.World;
 
-public  class EvileArmorBases {
+public class EvileArmorBases {
 
 	public static class AmethystArmorBase extends ArmorItem {
 
@@ -20,14 +20,13 @@ public  class EvileArmorBases {
 
 		@Override
 		public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
-			if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(1) != null
-					&& player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(3) != null) {
-				if (player.inventory.armorItemInSlot(3).getItem() == EvileDeferredRegistry.AMETHYST_HELMET.get()
-						&& player.inventory.armorItemInSlot(2).getItem() == EvileDeferredRegistry.AMETHYST_CHESTPLATE.get()
-						&& player.inventory.armorItemInSlot(1).getItem() == EvileDeferredRegistry.AMETHYST_LEGGINGS.get()
+			if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(1) != null && player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(3) != null) {
+				if (player.inventory.armorItemInSlot(3).getItem() == EvileDeferredRegistry.AMETHYST_HELMET.get() && player.inventory.armorItemInSlot(2).getItem() == EvileDeferredRegistry.AMETHYST_CHESTPLATE.get() && player.inventory.armorItemInSlot(1).getItem() == EvileDeferredRegistry.AMETHYST_LEGGINGS.get()
 						&& player.inventory.armorItemInSlot(0).getItem() == EvileDeferredRegistry.AMETHYST_BOOTS.get()) {
+
 					player.addPotionEffect(new EffectInstance(Effects.SPEED, 20, 0, false, false));
-					player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 20, 0, false, false));
+					player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 201, 0, false, false));
+
 				}
 			}
 		}
@@ -42,16 +41,15 @@ public  class EvileArmorBases {
 
 		@Override
 		public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
-			if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(1) != null
-					&& player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(3) != null) {
-				if (player.inventory.armorItemInSlot(3).getItem() == EvileDeferredRegistry.POWERED_AMETHYST_HELMET.get()
-						&& player.inventory.armorItemInSlot(2).getItem() == EvileDeferredRegistry.POWERED_AMETHYST_CHESTPLATE.get()
-						&& player.inventory.armorItemInSlot(1).getItem() == EvileDeferredRegistry.POWERED_AMETHYST_LEGGINGS.get()
+			if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(1) != null && player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(3) != null) {
+				if (player.inventory.armorItemInSlot(3).getItem() == EvileDeferredRegistry.POWERED_AMETHYST_HELMET.get() && player.inventory.armorItemInSlot(2).getItem() == EvileDeferredRegistry.POWERED_AMETHYST_CHESTPLATE.get() && player.inventory.armorItemInSlot(1).getItem() == EvileDeferredRegistry.POWERED_AMETHYST_LEGGINGS.get()
 						&& player.inventory.armorItemInSlot(0).getItem() == EvileDeferredRegistry.POWERED_AMETHYST_BOOTS.get()) {
+
 					player.addPotionEffect(new EffectInstance(Effects.SPEED, 20, 1, false, false));
-					player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 20, 0, false, false));
+					player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 201, 0, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING, 20, 0, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.HASTE, 20, 0, false, false));
+
 				}
 			}
 		}
@@ -66,20 +64,19 @@ public  class EvileArmorBases {
 
 		@Override
 		public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
-			if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(1) != null
-					&& player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(3) != null) {
-				if (player.inventory.armorItemInSlot(3).getItem() == EvileDeferredRegistry.OVER_POWERED_AMETHYST_HELMET.get()
-						&& player.inventory.armorItemInSlot(2).getItem() == EvileDeferredRegistry.OVER_POWERED_AMETHYST_CHESTPLATE.get()
-						&& player.inventory.armorItemInSlot(1).getItem() == EvileDeferredRegistry.OVER_POWERED_AMETHYST_LEGGINGS.get()
+			if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(1) != null && player.inventory.armorItemInSlot(2) != null && player.inventory.armorItemInSlot(3) != null) {
+				if (player.inventory.armorItemInSlot(3).getItem() == EvileDeferredRegistry.OVER_POWERED_AMETHYST_HELMET.get() && player.inventory.armorItemInSlot(2).getItem() == EvileDeferredRegistry.OVER_POWERED_AMETHYST_CHESTPLATE.get() && player.inventory.armorItemInSlot(1).getItem() == EvileDeferredRegistry.OVER_POWERED_AMETHYST_LEGGINGS.get()
 						&& player.inventory.armorItemInSlot(0).getItem() == EvileDeferredRegistry.OVER_POWERED_AMETHYST_BOOTS.get()) {
+
 					player.addPotionEffect(new EffectInstance(Effects.SPEED, 20, 2, false, false));
-					player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 20, 0, false, false));
+					player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 201, 0, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING, 20, 0, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.HASTE, 20, 1, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 20, 2, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.REGENERATION, 20, 20, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.STRENGTH, 20, 2, false, false));
 					player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 20, 0, false, false));
+
 				}
 			}
 		}
