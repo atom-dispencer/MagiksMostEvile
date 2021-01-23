a577f5c0f7fe55d3072bc65739bc54601d49035f
GenElectrovise <sanchez.pamelos@outlook.com>
2021-01-23 17:36:48 +0000

    Fixed preliminary broken model errors before testing

--- src/main/java/genelectrovise/magiksmostevile/item/equipment/tabula/Tabula.java
+++ src/main/java/genelectrovise/magiksmostevile/item/equipment/tabula/Tabula.java
@@ -1,14 +1,13 @@
 package genelectrovise.magiksmostevile.item.equipment.tabula;
 
 import genelectrovise.magiksmostevile.core.support.EvileItemGroup;
-import genelectrovise.magiksmostevile.registry.orbital.registries.ItemOrbitalRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.Rarity;
 
 public abstract class Tabula extends Item {
 
   public Tabula() {
-    super(new Item.Properties().maxStackSize(1).group(EvileItemGroup.ITEMGROUP_EVILE).rarity(Rarity.UNCOMMON).containerItem(ItemOrbitalRegistry.BLANK_TABLULA.get()));
+    super(new Item.Properties().maxStackSize(1).group(EvileItemGroup.ITEMGROUP_EVILE).rarity(Rarity.UNCOMMON));
   }
 
 }
