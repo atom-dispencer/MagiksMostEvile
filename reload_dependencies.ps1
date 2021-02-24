3fe8ac2752e17e8fbf26f329354e11a17a018657
GenElectrovise <sanchez.pamelos@outlook.com>
2021-02-24 11:47:39 +0000

    Updated to forge 1.16.5-36.0.42, magiksmostevile-MC_1.16.5_alpha_1.0_0004

---
+++ reload_dependencies.ps1
@@ -0,0 +1,5 @@
+@echo off
+echo "Greetings, Adventurer! Refreshing gradle dependencies! You'll need to refresh your gradle project after this command has completed!"
+./gradlew build --refresh-dependencies
+echo "Refresh either completed or crashed! Execution complete..."
+pause
