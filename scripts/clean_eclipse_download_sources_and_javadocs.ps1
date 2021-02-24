9ee5576c688c76b5f668d90200ca91aad2b3f783
GenElectrovise <sanchez.pamelos@outlook.com>
2021-02-24 12:48:08 +0000

    Added /scripts

---
+++ scripts/clean_eclipse_download_sources_and_javadocs.ps1
@@ -0,0 +1,4 @@
+echo "Greetings, Adventurer! Downloading sources and javadocs! You'll need to refresh your gradle project after this command has completed!"
+.././gradlew cleanEclipse eclipse
+echo "Refresh either completed or crashed! Execution complete..."
+pause
--- scripts/reload_dependencies.ps1
+++ scripts/reload_dependencies.ps1
@@ -1,5 +1,4 @@
-@echo off
 echo "Greetings, Adventurer! Refreshing gradle dependencies! You'll need to refresh your gradle project after this command has completed!"
-./gradlew build --refresh-dependencies
+.././gradlew build --refresh-dependencies
 echo "Refresh either completed or crashed! Execution complete..."
 pause
