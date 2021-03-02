e360679e677584d7fc9ed51dbf4f156278741f75
GenElectrovise <sanchez.pamelos@outlook.com>
2021-03-02 09:56:40 +0000

    Added MuddiedDust behavior. Added SpringBreeze behavior.

--- src/main/java/genelectrovise/magiksmostevile/item/equipment/tabulae/TabulaSpringBreeze.java
+++ src/main/java/genelectrovise/magiksmostevile/item/equipment/tabulae/TabulaSpringBreeze.java
@@ -1,11 +1,14 @@
 package genelectrovise.magiksmostevile.item.equipment.tabulae;
 
 import java.util.List;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentUtils;
 import net.minecraft.world.World;
@@ -18,6 +21,28 @@ public class TabulaSpringBreeze extends Tabula {
     // Only operate on the server
     if (world.isRemote) {
       return ActionResult.resultPass(stack);
+    }
+
+    // Make sure the player is actually holding the item!
+    if (!(stack.getItem() instanceof TabulaMuddiedDust)) {
+      return ActionResult.resultFail(stack);
+    }
+
+    // Logic
+    BlockPos position = player.getPosition();
+    int lowerLimit = -1;
+    int upperLimit = 1;
+    for (int x = position.getX() + lowerLimit; x < position.getX() + upperLimit + 1; x++) {
+      for (int z = position.getZ() + lowerLimit; z < position.getZ() + upperLimit + 1; z++) {
+
+        BlockPos movingPosition = new BlockPos(x, position.getY() - 1, z);
+        BlockState state = world.getBlockState(movingPosition);
+        // If dirt and 1/4 chance
+        if (state.getBlock() == Blocks.DIRT && (world.getRandom().nextBoolean()) && world.getRandom().nextBoolean()) {
+          world.setBlockState(movingPosition, Blocks.GRASS_BLOCK.getDefaultState(), 2);
+        }
+      }
+
     }
 
     return ActionResult.resultSuccess(stack);
