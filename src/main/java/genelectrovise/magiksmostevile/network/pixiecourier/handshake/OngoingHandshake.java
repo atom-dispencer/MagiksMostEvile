18440be90775ab724c9a96d943d0e0f03940207f
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-02-25 15:46:41 +0000

    Added Lombok to compile-only dependencies and lombokified OngoingHandshake

--- src/main/java/genelectrovise/magiksmostevile/network/pixiecourier/handshake/OngoingHandshake.java
+++ src/main/java/genelectrovise/magiksmostevile/network/pixiecourier/handshake/OngoingHandshake.java
@@ -1,31 +1,11 @@
 package genelectrovise.magiksmostevile.network.pixiecourier.handshake;
 
+import lombok.Data;
+
+@Data
 public class OngoingHandshake {
     protected long initiationTime;
     protected String clientRegistryHash;
     protected String serverResponse;
-
-    public long getInitiationTime() {
-        return initiationTime;
-    }
-
-    public void setInitiationTime(long initiationTime) {
-        this.initiationTime = initiationTime;
-    }
-
-    public String getClientRegistryHash() {
-        return clientRegistryHash;
-    }
-
-    public void setClientRegistryHash(String clientRegistryHash) {
-        this.clientRegistryHash = clientRegistryHash;
-    }
-
-    public String getServerResponse() {
-        return serverResponse;
-    }
-
-    public void setServerResponse(String serverResponse) {
-        this.serverResponse = serverResponse;
-    }
+    protected HandshakePhase handshakePhase;
 }
