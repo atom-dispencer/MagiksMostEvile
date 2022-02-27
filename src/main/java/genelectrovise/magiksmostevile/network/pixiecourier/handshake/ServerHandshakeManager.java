ecea368a8ef5ab45068fe8c02e52365284ce9d49
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-02-27 17:48:38 +0000

    Added @Data to CourierHandshakePacket. Changed comments in ServerHandshakeManager.

--- src/main/java/genelectrovise/magiksmostevile/network/pixiecourier/handshake/ServerHandshakeManager.java
+++ src/main/java/genelectrovise/magiksmostevile/network/pixiecourier/handshake/ServerHandshakeManager.java
@@ -5,14 +5,13 @@ import genelectrovise.magiksmostevile.network.pixiecourier.PixieCourier;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.network.NetworkEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 public class ServerHandshakeManager implements HandshakeManager {
 
@@ -20,8 +19,29 @@ public class ServerHandshakeManager implements HandshakeManager {
 
     protected volatile Map<UUID, OngoingHandshake> ongoingHandshakes = Maps.newHashMap();
 
+
+    /**
+     * When the event is fired, starts a new handshake interaction. Delegated from {@link PixieCourier}. Only called from the dedicated server using {@link net.minecraftforge.fml.DistExecutor#safeRunWhenOn(Dist, Supplier)}
+     *
+     * @param event {@link PlayerEvent.PlayerLoggedInEvent}
+     */
+    @Override
+    public void initiateNewHandshake(PlayerEvent.PlayerLoggedInEvent event) {
+        // Target must be a ServerPlayerEntity
+        if (!(event.getPlayer() instanceof ServerPlayerEntity)) {
+            return;
+        }
+
+        // Only called on dedicated server, so it should be working with ServerPlayerEntities.
+        ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+
+        // Send an empty exchange packet to the client
+        PixieCourier.getInstance().channel.send(net.minecraftforge.fml.network.PacketDistributor.PLAYER.with(() -> player), CourierHandshakePacket.getNewExchangePacket());
+    }
+
     @Override
     public CourierHandshakePacket getResponse(CourierHandshakePacket packet, OngoingHandshake ongoingHandshake, NetworkEvent.Context context) {
+
         //TODO Get server response to handshake
         return null;
     }
@@ -30,25 +50,5 @@ public class ServerHandshakeManager implements HandshakeManager {
     public OngoingHandshake getOngoingHandshake(ServerPlayerEntity sender) {
         UUID uuid = sender.getUUID();
         return ongoingHandshakes.get(uuid);
-    }
-
-
-
-    /**
-     * When the event is fired, starts a new handshake interaction.
-     *
-     * @param event {@link PlayerEvent.PlayerLoggedInEvent}
-     */
-    public void initiateNewHandshake(PlayerEvent.PlayerLoggedInEvent event) {
-        // Target must be a ServerPlayerEntity
-        if (!(event.getPlayer() instanceof ServerPlayerEntity)) {
-            return;
-        }
-
-        // This method is only called on the dedicated server, so it should be working with ServerPlayerEntities.
-        ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
-
-        // Send an empty exchange packet to the client
-        PixieCourier.getInstance().channel.send(net.minecraftforge.fml.network.PacketDistributor.PLAYER.with(() -> player), CourierHandshakePacket.getNewExchangePacket());
     }
 }
