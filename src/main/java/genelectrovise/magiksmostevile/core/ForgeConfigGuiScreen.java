f109c0e19279b1c728152a469eabea0af9ef4135
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-02-28 15:29:48 +0000

    Update: The ForgeConfigGuiScreen works but the game freezes and you have to escape out.

--- src/main/java/genelectrovise/magiksmostevile/core/ForgeConfigGuiScreen.java
+++ src/main/java/genelectrovise/magiksmostevile/core/ForgeConfigGuiScreen.java
@@ -3,9 +3,14 @@ package genelectrovise.magiksmostevile.core;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.StringTextComponent;
 
+/**
+ * Allows editing common and client config in game. Currently, freezes the game GUI and you have to ESC out.
+ */
 public class ForgeConfigGuiScreen extends Screen {
 
     public ForgeConfigGuiScreen() {
         super(new StringTextComponent("Magiks Most Evile Config"));
     }
+
+
 }
