7aac839455931c528fd86c387a4d38fc12f4a4db
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-02-28 21:45:18 +0000

    Added some stubbed methods to ForgeConfigGuiScreen, including immediate closing because it doesn't do anything, and we don't want people to get stuck.

--- src/main/java/genelectrovise/magiksmostevile/core/ForgeConfigGuiScreen.java
+++ src/main/java/genelectrovise/magiksmostevile/core/ForgeConfigGuiScreen.java
@@ -1,5 +1,6 @@
 package genelectrovise.magiksmostevile.core;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.StringTextComponent;
 
@@ -12,5 +13,35 @@ public class ForgeConfigGuiScreen extends Screen {
         super(new StringTextComponent("Magiks Most Evile Config"));
     }
 
+    @Override
+    protected void init() {
+        super.init();
+        // Close screen immediately because it doesn't do anything right now.
+        onClose();
+    }
 
+    @Override
+    public void removed() {
+        super.removed();
+    }
+
+    @Override
+    public void onClose() {
+         super.onClose();
+    }
+
+    @Override
+    public boolean keyPressed(int keyId, int p_231046_2_, int p_231046_3_) {
+        return super.keyPressed(keyId, p_231046_2_, p_231046_3_);
+    }
+
+    @Override
+    public void render(MatrixStack stack, int p_230430_2_, int p_230430_3_, float p_230430_4_) {
+        super.render(stack, p_230430_2_, p_230430_3_, p_230430_4_);
+    }
+
+    @Override
+    public void renderBackground(MatrixStack p_230446_1_) {
+        super.renderBackground(p_230446_1_);
+    }
 }
