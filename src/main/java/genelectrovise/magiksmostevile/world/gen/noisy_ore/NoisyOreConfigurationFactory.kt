7dd02c22b675450212823d3e271ae2d2aba7799a
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-04-16 17:22:03 +0100

    Added Kotlin to the project with a view to working in Kotlin from now on. This will be cool because I do not know Kotlin, though would very much like to learn.

---
+++ src/main/java/genelectrovise/magiksmostevile/world/gen/noisy_ore/NoisyOreConfigurationFactory.kt
@@ -0,0 +1,24 @@
+package genelectrovise.magiksmostevile.world.gen.noisy_ore;
+
+import net.minecraft.resources.IResourceManager
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+class NoisyOreConfigurationFactory {
+
+    companion object {
+
+        @JvmStatic
+        private val logger: Logger = LogManager.getLogger(NoisyOreConfigurationFactory::class)
+
+        @JvmStatic
+        fun fromResources(manager: IResourceManager): NoisyOreConfiguration? {
+
+            logger.info("Kotlin test logger")
+
+            return null
+        }
+    }
+
+
+}
