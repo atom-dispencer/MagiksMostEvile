01c546deef40a3cd9b25aba107a400a4027a3052
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-05-08 13:33:05 +0100

    Completed switch to Minecraft 1.18.2 and Parchment 1.18.2 mapp

--- src/main/java/genelectrovise/magiksmostevile/world/noisyore/NoisyOreConfigurationFactory.kt
+++ src/main/java/genelectrovise/magiksmostevile/world/noisyore/NoisyOreConfigurationFactory.kt
@@ -4,7 +4,7 @@ import com.google.common.collect.Maps
 import com.google.gson.JsonElement
 import com.google.gson.JsonSyntaxException
 import genelectrovise.magiksmostevile.gson.GsonConfigurator
-import net.minecraft.util.ResourceLocation
+import net.minecraft.resources.ResourceLocation
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
